253a0ccfc74bbe67dfe4fed436d204f620412bac
tchevaphatra <thaya.chevaphatrakul@student.unimelb.edu.au~>
2023-02-22 23:37:22 +1100

    updated resume

--- src/component/Main.js
+++ src/component/Main.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import './css/Main.css'
+import PDF from '../files/Thaya_IT_CV.pdf'
 
 export default function Main() {
     const words = [
         "A Web Developer & Tech Enthusiast.", 
-        "Welcome to my Portfolio",
-        "This page is made with React!"
+        "Welcome to my Portfolio!",
+        "This page is made with React."
     ]
     const [word, setWord] = React.useState(words[0])
     const [time, setTime] = React.useState(0)
@@ -44,11 +45,11 @@ export default function Main() {
             <div className="frame">
                 <img src={require("../img/port1.jpg")}></img>
             </div>
-            <div className="intro">
-                <h1 className='name'>Hi, <span className="highlight">I'm Thaya Chevaphatrakul</span></h1>
-                <p className="tagline" style={fade}>{`<>${word}</>`}</p>
-                <a className="mainBtn"><h4>My Resume</h4></a>
-            </div>
+                <div className="intro">
+                    <h1 className='name'>Hi, <span className="highlight">I'm Thaya Chevaphatrakul</span></h1>
+                    <p className="tagline" style={fade}>{`<>${word}</>`}</p>
+                    <a href={PDF} target="_blank"className="mainBtn"><h4>My Resume</h4></a>
+                </div>
             </div>
         </section>
     )
