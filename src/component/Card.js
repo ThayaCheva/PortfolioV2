ce2f84e75735ebce3d77311fcfa9b9c270eece2c
tchevaphatra <thaya.chevaphatrakul@student.unimelb.edu.au~>
2023-02-26 15:00:35 +1100

    fix project responsiveness

--- src/component/Card.js
+++ src/component/Card.js
@@ -13,10 +13,10 @@ export default function Card(props) {
             <div className="card-info">
               <div className="card-title">
                 <div className="bullet"></div>
-                <h2>{props.title}</h2>
+                <h2>{props.title}</h2>  
               </div>
                 <h3 className="time">{props.time}</h3>
-                <p>JobTrack allows you to keep track of your applications while job hunting. For each job you can view its details, keep a checklist and upload related files (e.g. resumes). You can also add your interviews to a calendar. The application is built with Laravel.</p>
+                <p>{props.desc}</p>
               <button className="btn code-btn"><FontAwesomeIcon icon={faGithub} className="code-icon"/><h4>Code</h4></button>
             </div>
           </div>
--- src/component/Projects.js
+++ src/component/Projects.js
@@ -5,32 +5,36 @@ import './css/Projects.css'
 export default function Projects() {
   const cards = [
     {
+      id: "3",
+      title:"Front-End Development",
+      time:"2018-2019",
+      img:require("../img/proj3.PNG"),
+      link:"https://github.com/ThayaCheva/Web-Development/tree/master/Made%20In%20Abyss%20Website_2",
+      desc:"This is a Front-End Development project made for my EPQ project as part of my A-levels. The website is based on the show Made In Abyss and functions as some-what of a guide."
+    },
+    {
       id: "1",
       title:"Tenzi Game",
       time:"2022-2023",
       img:require("../img/Tenzi.JPG"),
-      link:"https://github.com/ThayaCheva/React-Practice/tree/main/tenzies"
+      link:"https://github.com/ThayaCheva/React-Practice/tree/main/tenzies",
+      desc:"Tenzi is game where the goal is roll 10 dices until all the dices face are the same. The game is made using React and as part of my React practice project."
     },
     {
       id: "2",
       title:"Shadow Pirate",
       time:"2022",
       img:require("../img/shadow.JPG"),
-      link:"https://github.com/ThayaCheva/ShadowPirate"
-    },
-    {
-      id: "3",
-      title:"Front-End Development",
-      time:"2018-2019",
-      img:require("../img/proj3.PNG"),
-      link:"https://github.com/ThayaCheva/Web-Development/tree/master/Made%20In%20Abyss%20Website_2"
+      link:"https://github.com/ThayaCheva/ShadowPirate",
+      desc:"Shadow Pirate is top-down game made using Java and the Bagel library. This game is made as a project for Object Oriented Software Development (SWEN20003). The game consists of 2 stages and the goal is to reach the treasure chest at the end of stage 2. The game has 2 types of enemies that can shoot projectiles at the enemy and the player can melee attack."
     },
     {
       id: "4",
       title:"Hackthon Project", 
       time:"2022", 
       img:require("../img/port2.jpg"),
-      link:"https://github.com/ThayaCheva/Star-Wars-Final"
+      link:"https://github.com/ThayaCheva/Star-Wars-Final",
+      desc:"A game made using Java and the Bagel library as part of the Catalyst 2022 Hackathon. The game is made in 24 hours."
     }
   ]
 
@@ -40,6 +44,7 @@ export default function Projects() {
       time={card.time}
       img={card.img}
       link={card.link}
+      desc={card.desc}
     />
   )
 
