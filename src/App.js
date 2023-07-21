1af943575d53608d8a836f91a83a7e7d111c04ad
tchevaphatra <thaya.chevaphatrakul@student.unimelb.edu.au~>
2023-07-21 19:39:24 +1000

    made website responsive

--- src/App.js
+++ src/App.js
@@ -1,51 +1,27 @@
-import React from 'react'
+import Navbar from './Navbar.js'
+import About from './About.js'
+import Skills from './Skills.js'
+import Projects from './Projects.js'
+import Footer from './Footer.js'
 
-import Navbar from './component/Navbar.js'
-import Main from './component/Main.js'
-import About from './component/About.js'
-import Skills from './component/Skills.js'
-import Projects from './component/Projects.js'
-import Footer from './component/Footer.js'
-import './component/media/_media.css'
 
 function App() {
-  window.addEventListener('scroll', reveal);
-
-  function reveal() {
-    var reveals = document.querySelectorAll('.reveal')
-
-    for (var i = 0; i < reveals.length; i++) {
-      var windowheight = window.innerHeight
-      var revealtop = reveals[i].getBoundingClientRect().top
-      var revealpoint = 150
-
-      if (revealtop < windowheight - revealpoint) {
-        reveals[i].classList.add('active')
-      }
-      else {
-        reveals[i].classList.remove('active')
-      }
-    }
-  }
   return (
-    <div className="App">
+    <div>
       <Navbar/>
-      <Main/>
-      <div className="linebreak">
-        <div className="line"></div>
+      <div className="landing">
+        <div className="art">
+          <div className="art-container">
+            <div className="sun-container">
+              <img className="shrine" src={require("./images/shrine.png")}></img>
+            </div>
+            <img className="cloud" src={require("./images/cloud.png")} alt="cloud"></img>
+          </div>
+        </div>
       </div>
       <About/>
-      <div className="linebreak">
-        <div className="line"></div>
-      </div>
       <Skills/>
-      <div className="linebreak">
-        <div className="line"></div>
-      </div>
       <Projects/>
-      <div className="linebreak">
-        <div className="line"></div>
-      </div>
       <Footer/>
     </div>
   );
---
+++ src/Skills.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import './styling/skills.css'
+export default function Skills() {
+    return (
+        <section id="skills">
+            <div className="skills-container">
+                <div className="header">
+                    <img className="cross" src={require('./images/cross-black.png')}/>
+                    <h1>MY SKILLS</h1>
+                    <img className="cross" src={require('./images/cross-black.png')}/>    
+                </div>
+                <h2>Langauges:</h2>
+                <div className="skills-list">
+                    <div className="skills-item">PYTHON</div>
+                    <div className="skills-item">JAVA</div>
+                    <div className="skills-item">JAVASCRIPT</div>
+                    <div className="skills-item">C</div>
+                    <div className="skills-item">C#</div>
+                    <div className="skills-item">MYSQL</div>
+                </div>
+                <h2>Web Dev:</h2>
+                <div className="skills-list skills-2">
+                    <div className="skills-item">HTML</div>
+                    <div className="skills-item">CSS</div>
+                    <div className="skills-item">REACT</div>
+                    <div className="skills-item">SASS</div>
+                </div>
+            </div>
+            <img className="line-black" src={require("./images/circle-line-black.png")} alt="line-black"></img>
+            <img className="line-white" src={require("./images/circle-line-white.png")} alt="line-white"></img>
+        </section>
+    )
+}
