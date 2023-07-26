5e325e19d06eb78eceae0ba1ead9a914f537afcd
tchevaphatra <thaya.chevaphatrakul@student.unimelb.edu.au~>
2023-07-26 21:01:55 +1000

    fix project arrow

--- src/App.js
+++ src/App.js
@@ -3,13 +3,28 @@ import About from './About.js'
 import Skills from './Skills.js'
 import Projects from './Projects.js'
 import Footer from './Footer.js'
-
+import React from 'react'
 
 function App() {
+  const [parallaxPosition, setParallaxPosition] = React.useState({ x: 0, y: 0 });
+
+  const handleMouseMove = (e) => {
+    const moveValue = 0.05; // Adjust this value to control the intensity of the parallax effect
+    const x = (e.clientX - window.innerWidth / 2) * moveValue;
+    const y = (e.clientY - window.innerHeight / 2) * moveValue;
+    setParallaxPosition({ x, y });
+  };
   return (
     <div>
       <Navbar/>
       <div className="landing">
+        <div className="clouds" onMouseMove={handleMouseMove}>
+          <img className="cloud-object cloud-1" style={{
+          transform: `translate(${parallaxPosition.x}px, ${parallaxPosition.y}px)`,
+        }} src={require("./images/cloud1.png")}/>
+          <img className="cloud-object cloud-2" src={require("./images/cloud2.png")}/>
+          <img className="cloud-object cloud-3" src={require("./images/cloud3.png")}/>
+        </div>
         <div className="art">
           <div className="art-container">
             <img className="background" src={require("./images/wave-style-jap.jpg")} alt="cloud"></img>
